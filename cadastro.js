8487540e6444da45fe0e04827184ed5172f01b79
AMDRE <andlu-luz@hotmail.com>
2023-02-18 11:24:29 -0300

    Adicionando a funcionalidade de mostrar a mensagem

--- cadastro.js
+++ cadastro.js
@@ -2,3 +2,7 @@ function signUp(email) {
     return email;
 }
 //pegando o email cadastrado na plataforma.
+function mostraMensagem() {
+    return "Bem vindo ao sistema";
+}
+//adicionado função de mensagem
